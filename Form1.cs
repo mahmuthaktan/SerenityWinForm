6c27a80a9cd17408e9b562c8696c1578d204d0ad
mahmuthaktan <mahmuthaktan@gmail.com>
2016-12-08 09:32:23 +0300

    Using Serenity MVC framework for Windows Desktop

---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using SereneWınFormTest.Northwind.Entities;
+using SereneWınFormTest.Northwind.Repositories;
+using Serenity.Data;
+using Serenity.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerenityWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cs = SqlConnections.GetConnectionString("Northwind");
+
+            if (cs.Dialect.GetType() == typeof(OracleDialect))
+                return;
+
+            var cb = cs.ProviderFactory.CreateConnectionStringBuilder();
+            cb.ConnectionString = cs.ConnectionString;
+
+            string catalogKey = "?";
+            foreach (var ck in new[] { "Initial Catalog", "Database", "AttachDBFilename" })
+                if (cb.ContainsKey(ck))
+                {
+                    catalogKey = ck;
+                    break;
+                }
+
+            var catalog = cb[catalogKey] as string;
+            cb[catalogKey] = null;
+
+            var connection = SqlConnections.New(cb.ConnectionString, cs.ProviderName);
+
+     
+
+            var fld = NoteRow.Fields;
+
+            var listRequest = new ListRequest
+            {
+                ColumnSelection = ColumnSelection.List,
+                //EqualityFilter = new Dictionary<string, object>
+                //{
+                //    { fld.EntityType.PropertyName, handler.Row.Table },
+                //    { fld.EntityId.PropertyName, idField[handler.Row] ?? -1 }
+                //}
+            };
+
+            var notes = new NoteRepository().List(connection, listRequest).Entities;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Serenity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerenityWinForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var container = new MunqContainer();
+            Dependency.SetResolver(container);
+
+            Application.Run(new Form1());
+        }
+    }
+}
